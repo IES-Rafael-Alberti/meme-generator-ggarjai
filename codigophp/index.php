6bff0140fb1da6bee1fc5bbbdc5f12ef733a6204
ggarjai <ggarjai329@g.educaand.es>
2022-10-25 13:10:23 +0200

    preparacion de login

--- codigophp/index.php
+++ codigophp/index.php
@@ -1,3 +1,26 @@
+<?php
+//url for meme list
+$url = 'https://api.imgflip.com/get_memes';
+
+//open connection
+$ch = curl_init();
+
+//set the url
+curl_setopt($ch,CURLOPT_URL, $url);
+
+//So that curl_exec returns the contents of the cURL; rather than echoing it
+curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
+
+//receive url content 
+$result = curl_exec($ch);
+
+//decode content (assoc array)
+$data = json_decode($result, true);
+
+//if success shows images
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +30,20 @@
     <title>Página principal</title>
 </head>
 <body>
-    <a href="phpinfo.php">phpinfo()</a>
+
+<?php   
+if($data["success"]) {
+    //iterates over memes array
+    foreach($data["data"]["memes"] as $meme) {
+        //show meme image
+        echo "<input type='radio' id=".$meme['id']." name=['meme']  > <img width='50px' src='" . $meme['url'] . "'>";
+        
+    }
+}
+?>
+
+    <!-- <a href="phpinfo.php">phpinfo()</a>
     <a href="xdebug_info.php">xdebug_info()</a>
+    -->
 </body>
 </html>
